3757bb961c848fcec4677cf0499076884457dd57
徐书传 <xushuchuan@58.com>
2016-03-16 10:25:21 +0800

    base first commit

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by 徐书传 on 16/3/16.
+//  Copyright © 2016年 Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseProject
+//
+//  Created by 徐书传 on 16/3/16.
+//  Copyright © 2016年 Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
